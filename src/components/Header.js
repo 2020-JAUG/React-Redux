ee62723b9b1cd30f6b6c8095de600c48e9c7dfb2
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-18 16:44:17 +0100

    Import Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
  
 const Header = () => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-drak bg-primary justify-content-betwwen">
             <div className="container">
-                <h1>CRUD - React, Redux, REST API & Axios</h1>
+                <h1>
+                    <Link to={'/'} className="text-light">
+                        CRUD - React, Redux, REST API & Axios
+                    </Link>
+                </h1>
             </div>
 
-            <a href="/products/new"
+            <Link to={"/products/new"}
                 className="btn btn-danger nuevo-post d-block d-md-inline-block"
-            >Add Product &#43;</a>
+            >Add Product &#43;</Link>
 
         </nav>
      );
