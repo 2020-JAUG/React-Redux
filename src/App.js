1e5c0fd7bdd241f4944476af275e3df00c8479c3
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-18 16:10:37 +0100

    Import Products at App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Products from './components/Products';
 
 function App() {
   return (
