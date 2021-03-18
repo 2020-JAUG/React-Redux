d2c47a88333d7bafa3cfd69290198910ba7e34ab
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-18 17:34:17 +0100

    Create form to edit product

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -2,7 +2,44 @@ import React from 'react'
 
 const EditProduct = () => {
     return ( 
-        <h1>From ProductsEdit.js</h1>
+        <div className="row justify-content-center">
+             <div className="col-md-8">
+                 <div className="card">
+                     <div className="body">
+                         <h2 className="text-center mb-4 font-weight-bold">
+                            Edit Product
+                         </h2>
+
+                         <form>
+                            <div className="form-group">
+                                <label>Product Name</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    placeholder="Product Name"
+                                    name="name"
+                                />     
+                            </div> 
+
+                            <div className="form-group">
+                                <label>Product Price</label>
+                                <input
+                                    className="form-control"
+                                    type="number"
+                                    placeholder="Product Precio"
+                                    name="price"
+                                />     
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-bolock w-100"
+                                >Save Changes</button> 
+                         </form>
+                     </div>
+                 </div>
+             </div>
+         </div>
      );
 }
  
