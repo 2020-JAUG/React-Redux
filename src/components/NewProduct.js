4ce70528f1169f19da4948b94f7b3327716dd962
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-22 12:47:48 +0100

    We import Dispatch and create functions

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'; //useSelector accede al state dentro del componente
+import { createNewProductAction } from '../actions/productActions'; //Actions Redux
 
  const NewProdcut = () => {
+
+    //useDispatch executes the actions that we have in action
+    const dispatch = useDispatch();
+
+    //We call action from productActions
+    const addProduct = () => dispatch( createNewProductAction() ); 
+
+    //When the User do submit
+    const submitNewProduct = e => {
+        e.preventDefault();
+
+        //Validate form
+
+        //If there are no errors
+
+        //Create the new product
+        addProduct();
+
+
+    }
+
      return ( 
          <div className="row justify-content-center">
              <div className="col-md-8">
@@ -10,7 +33,9 @@ import React from 'react';
                              Add new Product
                          </h2>
 
-                         <form>
+                         <form
+                            onSubmit={submitNewProduct}
+                         >
                             <div className="form-group">
                                 <label>Product Name</label>
                                 <input
