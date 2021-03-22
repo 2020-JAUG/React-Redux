f6933229b07612349d9be4ddd2daf82344b7caff
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-22 13:48:33 +0100

    Add state to the form and validate them

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux'; //useSelector accede al state dentro del componente
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'; //useSelector accesses the state inside the component
 import { createNewProductAction } from '../actions/productActions'; //Actions Redux
 
  const NewProdcut = () => {
+
+    //State of component
+    const [name, saveName] = useState('');
+    const [price, savePrice] = useState(0);
+
 
     //useDispatch executes the actions that we have in action
     const dispatch = useDispatch();
 
     //We call action from productActions
-    const addProduct = () => dispatch( createNewProductAction() ); 
+    const addProduct = product => dispatch( createNewProductAction(product) );
 
     //When the User do submit
     const submitNewProduct = e => {
         e.preventDefault();
 
         //Validate form
-
+        if(name.trim() === '' || price <= 0) {
+            return;
+        }
         //If there are no errors
 
         //Create the new product
-        addProduct();
+        addProduct({
+            name,
+            price
+        });
 
 
     }
@@ -43,6 +53,8 @@ import { createNewProductAction } from '../actions/productActions'; //Actions Re
                                     type="text"
                                     placeholder="Product Name"
                                     name="name"
+                                    value={name}
+                                    onChange={e => saveName(e.target.value)}
                                 />     
                             </div> 
 
@@ -53,6 +65,8 @@ import { createNewProductAction } from '../actions/productActions'; //Actions Re
                                     type="number"
                                     placeholder="Product Precio"
                                     name="price"
+                                    value={price}
+                                    onChange={e => savePrice( Number(e.target.value) )}
                                 />     
                             </div>
 
