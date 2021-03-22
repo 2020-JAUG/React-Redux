43d3ae6eb2d60399a2fca05e9119b5980c5b23d7
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-22 11:40:49 +0100

    Delete imporst by error

--- src/reducers/productsReducers.js
+++ src/reducers/productsReducers.js
@@ -1,6 +1,3 @@
-import { createStore, applyMiddleware, compose } from 'redux'; //Con applyMiddleware pasamos thunk y lo agrega al store
-import thunk from 'redux-thunk';
-
 //Cada reducers tiene su propio started
 const initialState = {
     productos: [],
