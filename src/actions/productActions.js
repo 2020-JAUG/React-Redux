f8f6413c266d29b7e73bc57cba49daf350ca4a55
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-22 12:47:20 +0100

    Create function to the new products

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,2 +1,8 @@
-import { ADD__PRODUCT, ADD_PRODUCT_SUCCE, ADD_PRODUCT_ERROR } from '../types';
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCE, ADD_PRODUCT_ERROR } from '../types';
 
+//Create new products
+export function createNewProductAction() {
+    return () => {
+        console.log('from action')
+    }
+}
