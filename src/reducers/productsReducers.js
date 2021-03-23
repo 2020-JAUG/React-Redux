a2fdd85b5a2049ec792d701485622cde3f33caf9
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-23 16:12:18 +0100

    Add the new case. START_DOWNLOAD_PRODUCTS

--- src/reducers/productsReducers.js
+++ src/reducers/productsReducers.js
@@ -1,4 +1,11 @@
-import { ADD_PRODUCT, ADD_PRODUCT_SUCCE, ADD_PRODUCT_ERROR } from '../types';
+import { 
+    ADD_PRODUCT,
+    ADD_PRODUCT_SUCCE,
+    ADD_PRODUCT_ERROR,
+    START_DOWNLOAD_PRODUCTS,
+    DOWNLOAD_PRODUCTS_SUCCE,
+    DOWNLOAD_PRODUCTS_ERROR
+} from '../types';
 
 //Cada reducers tiene su propio started
 const initialState = {
@@ -10,6 +17,7 @@ const initialState = {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function( state = initialState, action ) {
     switch(action.type) {
+        case START_DOWNLOAD_PRODUCTS:
         case ADD_PRODUCT:
             return {...state, loading: action.payload}
 
