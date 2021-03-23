9b75a8e11f35a26c2199b996014c3ade7c150b43
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-23 10:11:24 +0100

    We create functions to modificate store

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,8 +1,32 @@
 import { ADD_PRODUCT, ADD_PRODUCT_SUCCE, ADD_PRODUCT_ERROR } from '../types';
 
-//Create new products
-export function createNewProductAction() {
-    return () => {
-        console.log('from action')
+//Create new products, and we call with useDispatch
+export function createNewProductAction(product) {
+    return (dispatch) => {
+        dispatch( addProduct() );
+
+        try {
+            dispatch( addProductSucce(product) );
+        } catch (error) {
+            dispatch( addProductError(true) );
+        }
     }
 }
+
+const addProduct = () => ({
+    type: ADD_PRODUCT,
+    payload: true
+});
+
+//If the product is saved in the database
+const addProductSucce = product => ({
+    type: ADD_PRODUCT_SUCCE,
+    payload: product
+})
+
+
+//If there was an error. state take the state of error
+const addProductError = state => ({
+    type: ADD_PRODUCT_ERROR,
+    payload: state
+})
