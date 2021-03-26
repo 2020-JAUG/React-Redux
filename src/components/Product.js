3c9681e223f50718ecece2761667fcb64b18868c
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-26 12:49:03 +0100

    Create new types to remove products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Product = ({product}) => {
+const Product = ( {product} ) => {
     const { name, price, id } = product
     return ( 
         <tr>
             <td>{name}</td>
-            <td><span className="font-weight-bold"> $ {price} </span></td>
+            <td><span className="font-weight-bold"> {price} € </span></td>
             <td className="actions">
-                <Link to={`/products/edit/${id}`} className="btn btn-primary mr-2">
-                    Edit
-                </Link>
-                <button type="button"
-                className="btn btn-danger"
-                >Delete</button>
+                <Link to={`/products/edit/${id}`} className="btn btn-primary mr-2"> Edit </Link>
+                <button type="button" className="btn btn-danger"> Delete </button>
             </td>
         </tr>
     )
