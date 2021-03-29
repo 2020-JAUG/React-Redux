bf57ae2f10a0c75a427df17df4f08ed526c5447c
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-29 10:48:26 +0200

    We add function remove products and cases

--- src/reducers/productsReducers.js
+++ src/reducers/productsReducers.js
@@ -4,14 +4,18 @@ import {
     ADD_PRODUCT_ERROR,
     START_DOWNLOAD_PRODUCTS,
     DOWNLOAD_PRODUCTS_SUCCE,
-    DOWNLOAD_PRODUCTS_ERROR
+    DOWNLOAD_PRODUCTS_ERROR,
+    GET_REMOVE_PRODUCT,
+    REMOVE_PRODUCT_SUCCE,
+    REMOVE_PRODUCT_ERROR
 } from '../types';
 
 //Cada reducers tiene su propio started
 const initialState = {
     products: [],
     error: null,
-    loading: false
+    loading: false,
+    removeproduct: null
 }
 //Here we evaluate the functions of actions
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -26,10 +30,17 @@ export default function( state = initialState, action ) {
 
         case ADD_PRODUCT_ERROR:
         case DOWNLOAD_PRODUCTS_ERROR:
+        case REMOVE_PRODUCT_ERROR:    
             return { ...state, loading: false, error: action.payload }
 
         case DOWNLOAD_PRODUCTS_SUCCE:
-            return { ...state, loading: false, error: null, products: action.payload }   
+            return { ...state, loading: false, error: null, products: action.payload }
+            
+        case GET_REMOVE_PRODUCT:
+            return { ...state, removeproduct: action.payload }
+
+        case REMOVE_PRODUCT_SUCCE:
+            return { ...state, products: state.products.filter(product => product.id !== state.removeproduct ), removeproduct: null }
 
         default:
             return state;
