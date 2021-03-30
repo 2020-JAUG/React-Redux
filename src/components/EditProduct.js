3fa36893b7bfb88361d3995e56bd6875a95e81da
alejandro urbina <urbinagonzalezja@gmail.com>
2021-03-30 18:19:33 +0200

    Add() to read the form data and new state of produ

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { editProductAction } from '../actions/productActions';
+import { useHistory } from 'react-router-dom';
 
 const EditProduct = () => {
 
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    //State new of product
+    const [ product, saveProduct ] = useState({
+        name: '',
+        price: ''
+    });
+
     //Product to edition
-    const product = useSelector( state => state.products.editproduct );
-    if( !product ) return null;
-    const { name, price, id } = product;
+    const editproduct = useSelector( state => state.products.editproduct );
+    
+    //Fill the state automatically
+    useEffect( () => {
+        saveProduct(editproduct);
+    }, [editproduct]);
+
+    //Read the form data
+    const onChangeForm = e => {
+        saveProduct({
+            ...product,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const { name, price } = product;
+
+    const submitEditProduct = e => {
+        e.preventDefault();
+
+       dispatch( editProductAction(product) );
+
+       history.push('/')
+    }
 
     return ( 
         <div className="row justify-content-center">
@@ -17,7 +49,7 @@ const EditProduct = () => {
                             Edit Product
                          </h2>
 
-                         <form>
+                         <form onSubmit={submitEditProduct}>
                             <div className="form-group">
                                 <label>Product Name</label>
                                 <input
@@ -26,6 +58,7 @@ const EditProduct = () => {
                                     placeholder="Product Name"
                                     name="name"
                                     value={name}
+                                    onChange={onChangeForm}
                                 />     
                             </div> 
 
@@ -37,6 +70,7 @@ const EditProduct = () => {
                                     placeholder="Product Precio"
                                     name="price"
                                     value={price}
+                                    onChange={onChangeForm}
                                 />     
                             </div>
 
